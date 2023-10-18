7cb6a5b40582b7b6419ca88a2d096d3ba60d8d8f
braharalex <uljador@gmail.com>
2023-10-18 23:22:45 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,29 +1,43 @@
+const numberInput = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesEl = document.getElementById("boxes");
+
+createBtn.addEventListener("click", onCreateBtnAmountOfBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function onCreateBtnAmountOfBoxes() {
+ const amount = Number(numberInput.value);
+
+ if (!amount) {
+  console.log("Кількість має бути більше 0");
+  return;
+ }
+
+ createBoxes(amount);
+}
+
+function createBoxes(amount) {
+ const boxes = [];
+ for (let i = 1; i <= amount; i += 1) {
+  const boxEl = document.createElement("div");
+  boxEl.style.width = "30px";
+  boxEl.style.width += "10px";
+  boxEl.style.height = "30px";
+  boxEl.style.height += "10px";
+  boxEl.style.backgroundColor = `${getRandomHexColor()}`;
+  console.log(boxEl);
+  boxes.push(boxEl);
+ }
+ boxesEl.append(...boxes);
+}
+
 function getRandomHexColor() {
  return `#${Math.floor(Math.random() * 16777215)
   .toString(16)
   .padStart(6, 0)}`;
 }
 
-const numberInput = document.querySelector('input[type="number"]');
-const createBtn = document.querySelector("button[data-create]");
-const destroyBtn = document.querySelector("button[data-destroy]");
-const boxesEl = document.getElementById("boxes");
-let number = 0;
-
-createBtn.addEventListener("click", onCreateBox);
-destroyBtn.addEventListener("click", destroyBoxes);
-
-function onCreateBox(event) {
- number = event.currentTarget.value;
- createBoxes(number);
-}
-// function onDestroyElements(event) {
-//   number = event.currentTarget.value;
-// }
-
-function createBoxes(amount) {}
-
-function destroyBoxes(event) {
- event.currentTarget.reset();
- e;
+function destroyBoxes() {
+ boxesEl.innerHTML = "";
 }
